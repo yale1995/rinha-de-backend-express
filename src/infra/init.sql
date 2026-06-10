6450afb09893b5071e99103dada0db65a1e872e4
Yale Araújo <yale850@gmail.com>
2026-06-10 14:06:27 -0300

    chore: create `pessoas` table schema and the `rinha_database` service in docker compose

---
+++ src/infra/init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS pessoas (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  apelido VARCHAR(32) UNIQUE NOT NULL,
+  nome VARCHAR(100) NOT NULL,
+  nascimento DATE NOT NULL,
+  stack VARCHAR(32)[]
+);
