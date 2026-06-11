5784fadcd1a4de2796c98d08caa88b4706f00763
Yale Araújo <yale850@gmail.com>
2026-06-11 12:11:59 -0300

    feat: implement `POST /pessoas` endpoint

--- src/server.js
+++ src/server.js
@@ -1,12 +1,21 @@
 import express from "express";
+import { client } from "./infra/database.js";
 
 const app = express();
 app.use(express.json());
 
 app.post("/pessoas", async (request, response) => {
-  const { body } = request;
+  const { apelido, nome, nascimento, stack } = request.body;
 
-  return response.json(request.body);
+  const result = await client.query(
+    "INSERT INTO pessoas (apelido, nome, nascimento, stack) VALUES ($1, $2, $3, $4) RETURNING id",
+    [apelido, nome, nascimento, stack]
+  );
+
+  const { id } = result.rows[0];
+
+  response.setHeader("Location", `/pessoas/${id}`);
+  return response.status(201).send();
 });
 
 app.listen(3000, () => {
