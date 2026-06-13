7b24df12ea55202209ff8e8771042c4e38a74c41
Yale Araújo <yale850@gmail.com>
2026-06-13 12:26:28 -0300

    chore: setup `api-image`, the `.dockerignore` and `api service` in `compose` file

--- src/infra/database.js
+++ src/infra/database.js
@@ -1,11 +1,11 @@
 import { Client } from "pg";
 
 export const client = new Client({
-  host: "localhost",
-  port: 5432,
-  user: "rinha",
-  database: "rinha",
-  password: "rinha",
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  user: process.env.DATABASE_USER,
+  database: process.env.DATABASE_NAME,
+  password: process.env.DATABASE_PASSWORD,
 });
 
 await client.connect();
